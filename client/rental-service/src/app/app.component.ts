fbfffe103c6b565737c2908ed107ea80bb02a840
Alexander Maslennikov <combat0705@yandex.ru>
2018-03-18 15:28:00 +0300

    Added semantic ui to project and reworked login component

--- client/rental-service/src/app/app.component.ts
+++ client/rental-service/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from "./auth.service";
 
 @Component({
@@ -9,5 +9,6 @@ import {AuthService} from "./auth.service";
 export class AppComponent {
   title = 'app';
 
-  constructor(public authService: AuthService){}
+  constructor(public authService: AuthService) {
+  }
 }
