af468dc1e6ece24156c0d1a7d2aef65d4cc7457c
Gigel773 <gigelbig@gmail.com>
2018-03-21 12:22:24 +0300

    Completed tests for UserController

--- rest/src/test/java/tests/UserControllerTests.java
+++ rest/src/test/java/tests/UserControllerTests.java
@@ -6,12 +6,13 @@ import com.nntu.controllers.UserController;
 import com.nntu.dao.UserDAO;
 import com.nntu.models.User;
 import helpers.UserTestInfo;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -23,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -31,7 +33,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class UserControllerTests {
     private MockMvc mvc;
     private UserTestInfo userTestInfo;
-    private UserTestInfo invalidUserTestInfo;
+    private User testRecord;
     private String SUCCESS = "SUCCESS";
     private String FAILURE = "FAILURE";
 
@@ -41,8 +43,9 @@ public class UserControllerTests {
     @InjectMocks
     private UserController userController;
 
-    @BeforeClass
+    @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         mvc = MockMvcBuilders.standaloneSetup(userController).build();
         userTestInfo = UserTestInfo.builder()
                 .id(1L)
@@ -52,33 +55,22 @@ public class UserControllerTests {
                 .password("123456")
                 .role(UserRole.CUSTOMER)
                 .build();
-        User testRecord = new User(userTestInfo.getName(),
+        testRecord = new User(userTestInfo.getName(),
                 userTestInfo.getLastName(),
                 userTestInfo.getEmail(),
                 UUID.nameUUIDFromBytes(userTestInfo.getPassword()
                         .getBytes())
                         .toString(),
                 UserRole.CUSTOMER);
-        testRecord.setId(userTestInfo.getId());
-        invalidUserTestInfo = UserTestInfo.builder()
-                .id(2L)
-                .name("Alex")
-                .lastName("Maslennikov")
-                .email("combat0705@yandex.ru")
-                .password("a12345")
-                .role(UserRole.LANDLORD)
-                .build();
-
-        Mockito.when(userDAO.findById(testRecord.getId()))
-                .thenReturn(Optional.of(testRecord));
-        Mockito.when(userDAO.getUserByEmail(testRecord.getEmail()))
-                .thenReturn(testRecord);
     }
 
     @Test
     public void registrationTest() throws Exception {
+        Mockito.when(userDAO.getUserByEmail(any(String.class)))
+                .thenReturn(null);
+
         MockHttpServletResponse response = mvc.perform(
-                post("/users/new-user" + invalidUserTestInfo))
+                post("/users/new-user" + userTestInfo))
                 .andReturn()
                 .getResponse();
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
@@ -87,6 +79,9 @@ public class UserControllerTests {
 
     @Test
     public void authorizationTest() throws Exception {
+        Mockito.when(userDAO.getUserByEmail(any(String.class)))
+                .thenReturn(testRecord);
+
         MockHttpServletResponse response = mvc.perform(
                 get("/users/authorize-user" + userTestInfo.authorizationString()))
                 .andReturn()
@@ -97,6 +92,9 @@ public class UserControllerTests {
 
     @Test
     public void userByIdTest() throws Exception {
+        Mockito.when(userDAO.findById(any(Long.class)))
+                .thenReturn(Optional.of(testRecord));
+
         MockHttpServletResponse response = mvc.perform(
                 get("/users/user-by-id" + userTestInfo.idString()))
                 .andReturn()
@@ -107,6 +105,9 @@ public class UserControllerTests {
 
     @Test
     public void registrationFailTest() throws Exception {
+        Mockito.when(userDAO.getUserByEmail(any(String.class)))
+                .thenReturn(testRecord);
+
         MockHttpServletResponse response = mvc.perform(
                 post("/users/new-user" + userTestInfo))
                 .andReturn()
@@ -117,8 +118,11 @@ public class UserControllerTests {
 
     @Test
     public void authorizationFailTest() throws Exception {
+        Mockito.when(userDAO.getUserByEmail(any(String.class)))
+                .thenReturn(null);
+
         MockHttpServletResponse response = mvc.perform(
-                get("/users/authorize-user" + invalidUserTestInfo.authorizationString()))
+                get("/users/authorize-user" + userTestInfo.authorizationString()))
                 .andReturn()
                 .getResponse();
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
@@ -127,8 +131,11 @@ public class UserControllerTests {
 
     @Test
     public void userByIdFailTest() throws Exception {
+        Mockito.when(userDAO.findById(any(Long.class)))
+                .thenReturn(Optional.empty());
+
         MockHttpServletResponse response = mvc.perform(
-                get("/users/user-by-id" + invalidUserTestInfo.idString()))
+                get("/users/user-by-id" + userTestInfo.idString()))
                 .andReturn()
                 .getResponse();
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
